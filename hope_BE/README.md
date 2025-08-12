d1fbbf10ed0eaae2c186791fa005bda397a90de2
csh3210 <sdafagew123@gmail.com>
2025-08-12 21:42:54 +0900

    feat:DB및 로그인, 가입 BE, FE연결(#8)

---
+++ hope_BE/README.md
@@ -0,0 +1,9 @@
+## 설치
+
+-   npm init -y
+-   npm i express dotenv cors
+-   npm i mongoose express-async-handler
+-   npm i bcrypt
+-   npm install mongoose
+-   npm install jsonwebtoken
+-   npm install --save-dev concurrently
---
+++ hope_BE/middlewares/checkLogin.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const jwtSecret = process.env.JWT_SECRET;
+
+const checkLogin = (req, res, next) => {
+    res.setHeader('Cache_Control', 'no-cache, no-store, must-revalidate');
+
+    module.exports = checkLogin;
+
+    const token = req.cookies.token;
+    if (!token) {
+        return res.redirect('/'); // 토큰이 없으면 로그인 페이지로 이동
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret); // 토큰 해석
+        req.user = decoded.username;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: '로그인이 필요합니다.' });
+    }
+};
