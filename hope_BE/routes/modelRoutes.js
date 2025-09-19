8ed6bb4c162f2afe04ac8c93e3fc5c79e0e05662
csh3210 <sdafagew123@gmail.com>
2025-09-20 00:12:47 +0900

    show_model

--- hope_BE/routes/modelRoutes.js
+++ hope_BE/routes/modelRoutes.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 const dataController = require('../controllers/dataController');
 const modelController = require('../controllers/modelController');
-const checkLogin = require('../middlewares/checkLogin'); // 로그인 확인 미들웨어
+const testController = require('../controllers/testController');
+const checkLogin = require('../middlewares/checkLogin');
 
 // 사용자 데이터 수집 (user_data.py 실행)
 router.post('/userdata', checkLogin, dataController.collect);
 
 // 사용자 모델 학습 (user_model.py 실행)
 router.post('/usermodel', checkLogin, modelController.train);
+
+// 모델 테스트 (test.py 실행)
+router.post('/test/start', checkLogin, testController.startTest);
+router.post('/test/stop', checkLogin, testController.stopTest);
 
 module.exports = router;
